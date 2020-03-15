fada69b665aeff70127f890c6938528fc433c5a0
TJark Lundt <tjark@lundt.eu>
2020-03-15 11:00:42 +0100

    Add simple example to ensure JavaFX functionality (Button does nothing)

--- src/main/Starter.java
+++ src/main/Starter.java
@@ -1,9 +1,26 @@
 package main;
 
-public class Starter {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Starter extends Application{
 
 	public static void main(String[] args) {
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Test");
+		Button btn = new Button();
+		btn.setText("Hello World");
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		stage.setScene(new Scene(root, 1280, 720));
+		stage.show();
 	}
 
 }
